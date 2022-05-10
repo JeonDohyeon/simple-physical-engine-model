2f328ec156c9844501575c468aa5aefa92d4de4b
Dohyeon Jeon <jdh59681@naver.com>
2022-05-10 11:28:57 +0900

    Update Vector.java

--- src/main/com/github/jeondohyeon/simplePhEngine/vector/Vector.java
+++ src/main/com/github/jeondohyeon/simplePhEngine/vector/Vector.java
@@ -1,9 +1,9 @@
 package com.github.jeondohyeon.simplePhEngine.vector;
 
-public class Vec2 {
-  private float x;
-  private float y;
-  private float z;
+public class Vec3 {
+  public float x;
+  public float y;
+  public float z;
   
   public static final float getX() {return self.x;}
   public static final float getY() {return self.y;}
@@ -13,9 +13,25 @@ public class Vec2 {
   public static final setY(float in) {self.y = in;}
   public static final setZ(float in) {self.z = in;}
   
-  public static final void set(float x, float y, float z) {
+  public static final set(float x, float y, float z) {
     self.setX(x);
     self.setY(y);
     self.setZ(z);
+  }
+  
+  public class Vec2 {
+    public float x;
+    public float y;
+  }
+  
+  public static final float getX() {return self.x;}
+  public static final float getY() {return self.y;}
+  
+  public static final setX(float in) {self.x = in;}
+  public static final setY(float in) {self.y = in;}
+  
+  public static final set(float x, float y) {
+    self.setX(x);
+    self.setY(y);
   }
 }
